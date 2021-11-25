8feb4cac63ce5b78c039177fa52a5a3d8e2772e3
jm96441n <john@johnmaguirdeveloper.com>
2021-11-24 22:34:09 -0500

    Successfully posting to notion

--- cli/sync.go
+++ cli/sync.go
@@ -5,9 +5,10 @@ import (
 	"context"
 	"encoding/json"
 	"flag"
-	"io"
+	"fmt"
 	"net/http"
 	"os/exec"
+	"time"
 
 	"github.com/peterbourgon/ff/v3/ffcli"
 )
@@ -23,8 +24,7 @@ type Config struct {
 }
 
 type httpInteractor interface {
-    Get(string) (*http.Response, error)
-    Post(string, string, io.Reader) (*http.Response, error)
+    Do(*http.Request) (*http.Response, error)
 }
 
 type JrnlBody struct {
@@ -39,7 +39,7 @@ type Entry struct {
 type NotionDocument struct {
     Parent ParentInfo `json:"parent"`
     Properties NotionProperties `json:"properties"`
-    Children []ChildInfo `json:"children"`
+    Children []BulletedListItem `json:"children"`
 }
 
 type ParentInfo struct {
@@ -51,19 +51,23 @@ type NotionProperties struct {
 }
 
 type NotionName struct {
-    Title []NotionText `json:"title"`
+    Title []NotionTitle `json:"title"`
 }
 
 type NotionTitle struct {
-    Text NotionText `json:"text"`
+    Text map[string]string `json:"text"`
+    Type *string `json:"type,omitempty"`
 }
 
-type NotionText struct {
-    Data map[string]string
+
+type BulletedListItem struct {
+    Object string `json:"object"`
+    Type string `json:"type"`
+    BulletedList ListItem `json:"bulleted_list_item"`
 }
 
-type ChildInfo struct {
-    Blocks []BulletedListItems
+type ListItem struct {
+    Text []NotionTitle `json:"text"`
 }
 
 func NewSyncFlagSet(httpClient httpInteractor, output *bytes.Buffer, cmd *exec.Cmd) *ffcli.Command {
@@ -83,18 +87,105 @@ func NewSyncFlagSet(httpClient httpInteractor, output *bytes.Buffer, cmd *exec.C
 }
 
 func (c *Config) Exec(_ context.Context, _ []string) error {
+    entriesGroupedByDate, err := c.getEntriesGroupedByDate()
+    if err != nil {
+        return err
+    }
+    notionDocument := c.buildNotionDocument(entriesGroupedByDate)
+    err = c.postToNotion(notionDocument)
+    if err != nil {
+        return err
+    }
+    return nil
+}
+
+func (c *Config) getEntriesGroupedByDate() (map[string][]string, error) {
     c.cmd.Stdout = c.out
     err := c.cmd.Run()
     if err != nil {
-        return err
+        return nil, err
     }
     jrnlResp := JrnlBody{}
     err = json.Unmarshal(c.out.Bytes(), &jrnlResp)
     if err != nil {
-        return err
+        return nil, err
     }
     groupByDate := make(map[string][]string)
+    fmt.Println(len(jrnlResp.Entries))
     for _, e := range jrnlResp.Entries {
         groupByDate[e.Date] = append(groupByDate[e.Date], e.Body)
     }
+
+    return groupByDate, nil
+}
+
+func (c *Config) buildNotionDocument(entriesGroupedByDate map[string][]string) NotionDocument {
+    children := make([]BulletedListItem, 0)
+
+    time := time.Now().Format("2006-01-02")
+    txt := "text"
+    todaysEntries := entriesGroupedByDate[time]
+    for _, e := range todaysEntries {
+        item := BulletedListItem{
+                Object: "block",
+                Type: "bulleted_list_item",
+                BulletedList: ListItem{
+                    Text: []NotionTitle{
+                        {
+                            Type: &txt,
+                            Text: map[string]string{"content": e},
+                        },
+                    },
+                },
+            }
+        children = append(children, item)
+    }
+
+    n := NotionDocument{
+        Parent: ParentInfo{DatabaseID: c.DBID},
+        Properties: NotionProperties{
+            Name: NotionName{
+                Title: []NotionTitle{
+                    {
+                        Text: map[string]string{
+                            "content": time,
+                        },
+                    },
+                },
+            },
+        },
+        Children: children,
+    }
+    return n
+}
+
+func (c *Config) postToNotion(notionDocument NotionDocument) error {
+    jsonBytes, err := json.Marshal(notionDocument)
+    if err != nil {
+        return err
+    }
+    buf := bytes.NewBuffer(jsonBytes)
+
+    url := fmt.Sprintf("%s/pages", notionBaseAddress)
+    req, err := http.NewRequest("POST", url, buf)
+    if err != nil {
+        return err
+    }
+
+    req.Header = http.Header{
+        "Content-Type": []string{"application/json"},
+        "Authorization": []string{fmt.Sprintf("Bearer %s", c.NotionKey)},
+        "Notion-Version": []string{"2021-08-16"},
+    }
+
+    res, err := c.httpClient.Do(req)
+    if err != nil {
+        return err
+    }
+
+    if res.StatusCode > 299 {
+        return fmt.Errorf("posting to notion failed with %d", res.StatusCode)
+    }
+    return nil
+
 }
